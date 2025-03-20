ea6e748ef2acfce9cd5fb8102eaa9e9aee8bebf2
Suraj Suseelan <surajsuseelan@Surajs-MacBook-Pro.local>
2025-03-20 12:31:43 +0530

    final chages

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 def add (a,b,c):
     return a+b+c
 
-<<<<<<< HEAD
 def sub (a,b,d):
     return a-b-d
-=======
-def sub (a,b,c):
-    return a-b-c
->>>>>>> 5e0ce70 (final chages)
 
 def mul (a,b):
     return a*b
